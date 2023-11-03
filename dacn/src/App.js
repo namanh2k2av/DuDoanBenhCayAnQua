8a2d59a31c8ae42ba46e34ebee40135a853020db
ngduchao <duchao0202@gmail.com>
2023-11-03 16:29:02 +0700

    reactjs

---
+++ dacn/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NhanDien from './component/nhanDien';
+
+function App() {
+  return (
+    <div>
+      <NhanDien/>
+    </div>
+  );
+}
+
+export default App;
+
+{/* react-lazy-load-image-component */}
